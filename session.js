54768c8a6d9c3217a2c372645732070e1f089c12
asitha-vercel <asithavercel@gmail.com>
2026-06-30 07:13:40 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1782783374510_rcxpyxdxy",
 PORT: process.env.PORT || "8000"
 };
